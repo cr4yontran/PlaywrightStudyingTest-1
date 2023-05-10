7f7b530ba7ca1ce44e0e49237c10d0bc048cb999
nguyen minh tri <man220715@gmail.com>
2023-05-10 14:03:49 +0700

    first push

---
+++ tests/example.2.spec.js
@@ -0,0 +1,14 @@
+const { test, expect } = require("@playwright/test");
+import ENV from "../utils/env";
+
+test("test 1", async ({ page }) => {
+  await page.goto(ENV.BASE_URL);
+  console.log(page.url());
+  console.log(ENV.TEXT);
+});
+
+test("test 2", async ({ page }) => {
+  await page.goto(ENV.BASE_URL);
+  console.log(page.url());
+  console.log(ENV.TEXT);
+});
---
+++ utils/globalSetup.ts
@@ -0,0 +1,12 @@
+import { FullConfig } from "@playwright/test";
+import dotenv from "dotenv";
+async function globalSetup(config: FullConfig) {
+  const test_env = process.env.test_env || "test";
+  if (test_env) {
+    dotenv.config({
+      path: `.env.${test_env}`,
+      override: true,
+    });
+  }
+}
+export default globalSetup;
