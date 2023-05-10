79a7129f1b59aca18dbf6134f00d15321c0a4e27
nguyen minh tri <man220715@gmail.com>
2023-05-10 14:24:53 +0700

    2nd push

--- tests/example.2.spec.js
+++ tests/example.2.spec.js
@@ -6,7 +6,7 @@ test("test 1", async ({ page }) => {
   console.log(page.url());
   console.log(ENV.TEXT);
 });
-
+ 
 test("test 2", async ({ page }) => {
   await page.goto(ENV.BASE_URL);
   console.log(page.url());
