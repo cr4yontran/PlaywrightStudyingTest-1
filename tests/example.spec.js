3268c819813af3ac35deddbe6a7c5c618a06cd54
nguyen minh tri <man220715@gmail.com>
2023-05-15 14:10:55 +0700

    commit

---
+++ tests/example.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require("@playwright/test");
+
+test("input fiedlds", async ({ page }) => {
+  
+    //go to website
+
+  await page.goto("https://demoqa.com/text-box");
+
+  // execute the commands
+  
+  await page.getByPlaceholder("Full Name").fill("Nguyen Minh Tri");
+  await page.getByPlaceholder("namw@example.com").fill("123@gmail.com");
+  await page.getByPlaceholder("Current Address").fill("Berlin");
+  await page.getByText("Submit").click();
+
+  // assertion
+
+  await expect(page.getByText("Name:")).toContainText("Nguyen Minh Tri");
+  await expect(page.getByText("Email:")).toContainText("123@gmail.com");
+  await expect(page.getByText("Current Address :")).toContainText("Berlin");
+});
